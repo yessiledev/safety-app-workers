1899befb5bd4e1810beebb1ef302e795896f8d73
yessiledev <cecilebenet768@gmail.com>
2026-06-01 16:01:19 +0200

    Update ZonesInterditesScreen.js

--- ZonesInterditesScreen.js
+++ ZonesInterditesScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import EmergencyButton from '../components/EmergencyButton';
 
 export default function ZonesInterditesScreen() {
   const navigation = useNavigation();
@@ -78,6 +79,7 @@ export default function ZonesInterditesScreen() {
       >
         <Text style={styles.buttonSecondaryText}>Signaler un danger</Text>
       </TouchableOpacity>
+<EmergencyButton />
 
     </ScrollView>
   );
