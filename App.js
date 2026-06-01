65d9b05f268c8797a16796d04912300060bb7b54
yessiledev <cecilebenet768@gmail.com>
2026-06-01 16:10:34 +0200

    Update App.js

--- App.js
+++ App.js
@@ -3,3 +3,5 @@ import Navigation from './src/navigation/Navigation';
 export default function App() {
   return <Navigation />;
 }
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useEffect, useState } from 'react';
